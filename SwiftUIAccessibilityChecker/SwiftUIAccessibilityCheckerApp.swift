2e8d35f1251587290ae298a71ce411696833e6df
azharuddin <azharuddin@barrierbreak.com>
2026-07-30 16:56:30 +0530

    Initial Commit

---
+++ SwiftUIAccessibilityChecker/SwiftUIAccessibilityCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccessibilityCheckerApp.swift
+//  SwiftUIAccessibilityChecker
+//
+//  Created by Azharuddin   on 30/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccessibilityCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
